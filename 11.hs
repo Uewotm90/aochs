d340a2e2e470262928014b956e742fb577855290
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2025-12-01 12:33:57 +0100

    bump

--- 11.hs
+++ 11.hs
@@ -1,3 +1,4 @@
+import Debug.Trace
 import Parsing
 
 p =
@@ -10,26 +11,48 @@ p =
       num <- int
       space
       return [toInteger num]
+
 splitInteger :: Integer -> [Integer]
 splitInteger x = pair (splitAt (length s `div` 2) s)
-    where
-        s = show x
-        pair (a,b) = [read a,read b]
+  where
+    s = show x
+    pair (a, b) = [read a, read b]
+
 evenDigit x = even (length $ show x)
 
 transform :: Integer -> [Integer]
-transform n | n == 0 = [1]
-            | evenDigit n = splitInteger n
-            | otherwise = [n * 2024]
+transform n
+  | n == 0 = [1]
+  | evenDigit n = splitInteger n
+  | otherwise = [n * 2024]
 
 blink xs = concatMap transform xs
-testInput = [0,1,10,99,999]
-testinput' = [125,17]
 
+testInput = [0, 1, 10, 99, 999]
+
+testinput' = [125, 17]
 
 blinks 0 xs = xs
-blinks n xs = blinks (n-1) $ blink xs
+blinks n xs = blinks (n - 1) $ blink xs
 
 firstSol = do
-    input <- readFile "./input/11.txt"
-    return $ length $ blinks 25 $ fst $ head $ parse p input
+  input <- readFile "./input/11.txt"
+  return $ length $ blinks 25 $ fst $ head $ parse p input
+
+transformStep2 :: Integer -> [Integer]
+transformStep2 n
+  | n == 0 = [2024]
+  | evenDigit n = [20, 24]
+  | otherwise = transform $ n * 2024
+
+blinkStep2 xs = concatMap transformStep2 xs
+
+blinks' :: Int -> [Integer] -> [Integer]
+blinks' n xs
+  | n == 0 = xs
+  | n == 1 = blink xs
+  | n > 1 = blinks' (n - 2) $ blinkStep2 xs
+
+secondSol = do
+  input <- readFile "./input/11.txt"
+  return $ length $ blinks' 75 $ fst $ head $ parse p input
