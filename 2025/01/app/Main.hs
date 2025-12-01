a3fc8863bc38a1759e2de1c00de6813a2204899d
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2025-12-01 16:46:50 +0100

    bump

--- 2025/01/app/Main.hs
+++ 2025/01/app/Main.hs
@@ -12,17 +12,27 @@ main = do
   print pwd
   input <- readFile "./input/input.txt"
   --   input <- test
-  print $ solve $ parse Main.lines "input" input
+  print $ solve $ parse Main.lines "input" test
+  print $ solve2 $ parse Main.lines "input" test
 
 test = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82\n"
 
 -- solve :: Either a Input -> Int
 solve (Left a) = undefined
-solve (Right b) = length . filter (==0) $ reverse $ foldr (\c acc -> rotateDial (head acc) c : acc) [50] $ reverse b
+solve (Right b) = length . filter (== 0) $ foldr (\c acc -> rotateDial (head acc) c : acc) [50] $ reverse b
+
+solve2 (Left _) = undefined
+solve2 (Right b) = foldr (\(a, b) acc -> acc + b + if a == 0 then 1 else 0) 0 $ foldr (\c acc -> rotateDial2 (fst $ head acc) c : acc) [(50, 0)] $ reverse b
 
 rotateDial :: Int -> (Char, Int) -> Int
-rotateDial d ('L', n) = if d >= n then (d - n `rem` 100) + 100 `mod` 100 else 100 - (n - d)
+rotateDial d ('L', n)
+  | d >= n = d - n
+  | otherwise = (d - n) `mod` 100
 rotateDial d ('R', n) = (d + n) `rem` 100
+
+rotateDial2 :: Int -> (Char, Int) -> (Int, Int)
+rotateDial2 d r@('L', n) = (rotateDial d r, abs $ (d - n) `div` 100)
+rotateDial2 d r@('R', n) = (rotateDial d r, (d + n) `div` 100)
 
 -- test = print ( parse line "test" "R1")
 -- parsing
